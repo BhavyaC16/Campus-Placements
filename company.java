aef6b439fa42911e5cb05b51dae08a39739011eb
Bhavya <bhavya18333@iiitd.ac.in>
2019-08-11 14:27:57 +0530

    added attributes and methods for class company

--- company.java
+++ company.java
@@ -1,3 +1,45 @@
 public class company{
-    
+    final private String name;
+    final private int no_eligible_courses;
+    final private int students_required;
+    final private String[] eligible_courses; 
+    private boolean application_status;
+
+    public company(String name, int no_eligible_courses, int students_required, String[] eligible_courses)
+    {
+        this.name = name; 
+        this.students_required = students_required;
+        this.no_eligible_courses = no_eligible_courses;
+        this.eligible_courses = new String[no_eligible_courses];
+        for(int i = 0;i<no_eligible_courses;i++)
+        {
+            this.eligible_courses[i] = eligible_courses[i]; 
+        }
+        this.application_status = true;
+    }
+
+    public void display_company()
+    {
+        System.out.println("Company Name: "+this.name);
+        System.out.println("Course Criteria: ");
+        for(int i = 0; i<this.no_eligible_courses;i++)
+        {
+            System.out.println(eligible_courses[i]);
+        }
+        System.out.print("Number of required students: ");
+        System.out.println(this.students_required);
+        if(this.application_status==true)
+        {
+            System.out.println("OPEN");
+        }
+        else{
+            System.out.println("CLOSED");
+        }
+    }
+    public static void main(String[] args)
+    {
+        String[] course = new String[]{"CSE","CSD"};
+        company c  = new company("ABC", 2, 5, course);
+        c.display_company();
+    }
 }
