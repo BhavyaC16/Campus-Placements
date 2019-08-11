024c69ddebcc43ccc028fd8084dc3a1cbda08283
Bhavya <bhavya18333@iiitd.ac.in>
2019-08-11 16:51:22 +0530

    Added attributes and methods, main method for placement office

--- placementOffice.java
+++ placementOffice.java
@@ -1,3 +1,89 @@
+import java.util.Scanner;
+import java.io.*; 
+import java.util.*;
+
 public class placementOffice{
-	
+	final private int students;
+	private int students_placed;
+	private static ArrayList<student> Students;
+	private static ArrayList<company> Companies;
+	private static ArrayList<student> removedStudents;
+	private static ArrayList<company> removedCompanies;
+
+	public placementOffice(int N)
+	{
+		this.students = N;
+		this.students_placed = 0;
+		this.Students = new ArrayList<student>(N);
+		this.Companies = new ArrayList<company>(N);
+		this.removedStudents = new ArrayList<student>(N);
+		this.removedCompanies = new ArrayList<company>(N);
+	}
+
+	public int get_no_placed_students()
+	{
+		return(this.students_placed);
+	}
+	public int get_no_unplaced_students()
+	{
+		return(this.students - this.students_placed);
+	}
+	public void add_company()
+	{
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.print("Number of Eligible Courses = ");
+		int no_eligible_courses = sc.nextInt();
+		String[] courses = new String[no_eligible_courses];
+		for(int k = 0;k<no_eligible_courses;k++) //Taking 1 input less.. solve
+		{
+			String s = sc.nextLine();
+			courses[k] = s;
+		}
+		System.out.print("Number of Required Students = ");
+		int required_students = sc.nextInt();
+
+		company c = new company(name, no_eligible_courses, required_students, courses);
+		c.display_company();
+
+	}
+
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		placementOffice pmo = new placementOffice(N);
+		for(int i=0;i<N;i++)
+		{
+			float cgpa = sc.nextFloat();
+			String branch = sc.nextLine();
+			student s = new student(cgpa, branch);
+			Students.add(s);
+		}
+		while(pmo.get_no_placed_students()!=N)
+		{
+			int ch = sc.nextInt();
+			switch(ch){
+				case 1:	pmo.add_company();
+						break;
+				case 2:
+						break;
+				case 3:
+						break;
+				case 4:
+						break;
+				case 5:
+						break;
+				case 6:
+						break;
+				case 7:
+						break;
+				case 8:
+						break;
+				case 9:
+						break;
+				default:
+						break;
+			}
+		}	
+	}
 }
